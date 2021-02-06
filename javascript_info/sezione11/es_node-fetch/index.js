74a8da8f67d84f17509416a9a0e74a7c82202d16
MatteoBartoli03 <matteobartoli01@gmail.com>
2021-02-06 07:40:43 +0100

    corrections

--- javascript_info/sezione11/es_node-fetch/index.js
+++ javascript_info/sezione11/es_node-fetch/index.js
@@ -1,11 +1,15 @@
 //due requests e poi gestisce e stampa una parte dei json restituitica
 const fetch = require('node-fetch')
 
-const r1 = fetch('https://jsonplaceholder.typicode.com/comments/1')
+const r1 = fetch('https://jsonplaceholder.typicode.com/comments/1', {
+  method : "GET",
+})
   .then(response => response.json())
   .then(json => json.email)
 
-const r2 = fetch('https://jsonplaceholder.typicode.com/comments/2')
+const r2 = fetch('https://jsonplaceholder.typicode.com/comments/2', {
+  method : "GET",
+})
     .then(response => response.json())
     .then(json => json.email)
 
