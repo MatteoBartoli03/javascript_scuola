7349d0339485c752a7b46ef4ea00c1c52df11511
FrancescoMusi <francesco.musi03@gmail.com>
2021-08-14 15:59:25 +0200

    aggiunta possibilita di vedere la home solo se loggati

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express') 
-const app = express() 
+var session = require('express-session');
 const bodyParser = require('body-parser')
 const path = require('path')
 const mysql = require('mysql')
 
+const app = express() 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -24,13 +25,24 @@ db.connect((err) => {
 })
 
 
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+
 //pages
 app.get('/', (req, res) => {
-  res.redirect('/login');
+  res.redirect('/home');
 });
 
 app.get('/home', (req, res) => {
-  res.sendFile(__dirname + '/pages/home.html');
+  if (req.session.loggedin) {
+    res.sendFile(__dirname + '/pages/home.html');
+	} else {
+		res.sendFile(__dirname + '/pages/errors/notLoggedIn.html');
+	}
 });
 
 app.get('/login', (req, res) => {
@@ -44,8 +56,7 @@ app.post('/login', (req, res) => {
   if (email && password) {
 		db.query('SELECT * FROM students WHERE email = ? AND password = ?', [email, password], (error, results, fields) => {
 			if (results.length > 0) {
-				//request.session.loggedin = true;
-				//request.session.username = username;
+				req.session.loggedin = true;
 				res.redirect('/home');
 			} else {
         res.redirect('back');
@@ -59,7 +70,7 @@ app.post('/login', (req, res) => {
 
 //deve stare ultimo
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/pages/pageNotFound.html');
+  res.sendFile(__dirname + '/pages/errors/pageNotFound.html');
 });
 
 
